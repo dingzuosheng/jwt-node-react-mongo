5d7f864666ec6a0f54849c48910b85782741675e
ding <zuosheng.ding@adesso.de>
2025-01-03 13:21:09 +0100

    node-react-mongo: extended header with reactive login and logout

--- jwt-react/src/pages/header/Header.js
+++ jwt-react/src/pages/header/Header.js
@@ -1,17 +1,36 @@
 import React from 'react'
 import {Container, Nav, Navbar} from "react-bootstrap";
-import {Link} from "react-router-dom";
+import {Link, useNavigate} from "react-router-dom";
 import "./Header.css";
 
 const Header = () => {
+    const token = localStorage.getItem("token");
+    const navigate = useNavigate();
+
+    const handleLogout = () => {
+        localStorage.removeItem("token");
+        navigate("/login");
+    }
+
     return (
         <>
-            <Navbar bg="dark" data-bs-theme="dark">
+            <Navbar bg={token ? "primary" : "dark"} data-bs-theme="dark">
                 <Container>
-                    <Navbar.Brand><strong>Not-LoggedIn</strong></Navbar.Brand>
+                    <Navbar.Brand as={Link} to = "">
+                        {token ? "Logged-In" : "Not-LoggedIn"}
+                    </Navbar.Brand>
                     <Nav className="ml-auto">
-                        <Nav.Link as={Link} to="/login" className='nav-link'>Login</Nav.Link>
-                        <Nav.Link as={Link} to="/register" className='nav-link'>Signup</Nav.Link>
+                        {token ? (
+                            <>
+                                <Nav.Link as={Link} to="/dashboard" className='nav-link'>Dashboard</Nav.Link>
+                                <Nav.Link className='nav-link' onClick={handleLogout} >Logout</Nav.Link>
+                            </>
+                        ) : (
+                            <>
+                                <Nav.Link as={Link} to="/login" className='nav-link'>Login</Nav.Link>
+                                <Nav.Link as={Link} to="/register" className='nav-link'>Signup</Nav.Link>
+                            </>
+                        )}
                     </Nav>
                 </Container>
             </Navbar>
