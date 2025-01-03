2e159f3625be80e65ac62fefce540de426564a55
ding <zuosheng.ding@adesso.de>
2025-01-03 10:00:06 +0100

    built functions for signup page and added cors dependency

--- jwt-react/src/pages/auth/signup/Signup.js
+++ jwt-react/src/pages/auth/signup/Signup.js
@@ -1,11 +1,31 @@
-import React from 'react'
+import React, {useState} from 'react'
 import {Button, Form} from "react-bootstrap";
 import "./Signup.css";
 
 const Signup = () => {
+  const[formData, setFormData] = useState({
+      email: '',
+      name: '',
+      password: ''
+  });
+
+  const handleInputChange = (event) => {
+      const { name, value} = event.target;
+      setFormData({
+          ...formData,
+          [name]: value
+      })
+  }
+
+  const handleSubmit = async (e) => {
+      e.preventDefault();
+      console.log("email: ", formData.email)
+      console.log("name: ", formData.name)
+      console.log("password: ", formData.password)
+  }
   return (
     <div className='center-form'>
-      <Form>
+      <Form onSubmit={handleSubmit}>
           <h1>Signup</h1>
           <Form.Group controlId="formBasicEmail">
               <Form.Label>Email address</Form.Label>
@@ -13,6 +33,18 @@ const Signup = () => {
                 type="email"
                 name="email"
                 placeholder="Enter email"
+                value={formData.email}
+                onChange={handleInputChange}
+              />
+          </Form.Group>
+          <Form.Group controlId="formBasicName">
+              <Form.Label>Name</Form.Label>
+              <Form.Control
+                  type="name"
+                  name="name"
+                  placeholder="Enter Name"
+                  value={formData.name}
+                  onChange={handleInputChange}
               />
           </Form.Group>
           <Form.Group controlId="formBasicPassword">
@@ -21,6 +53,8 @@ const Signup = () => {
                   type="password"
                   name="password"
                   placeholder="Enter password"
+                  value={formData.password}
+                  onChange={handleInputChange}
               />
           </Form.Group>
           <Button variant="dark" type="submit" className="w-100">
